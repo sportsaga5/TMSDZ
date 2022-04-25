c8aa52de3021c26507f39ac26a3a4cdd31b3b25b
sportsaga5 <sportsaga5@yandex.ru>
2022-04-25 21:01:01 +0500

    zd7

--- zadanye7.py
+++ zadanye7.py
@@ -1,5 +1,6 @@
 import json
 import csv
+import re
 
 # задание 1
 def json_fail():
@@ -20,29 +21,55 @@ def csv_fail():
 json_fail()
 csv_fail()
 
-# задание 2 
+# задание 2
+
+users_file = 'users.txt'
+
+def validate_credentials(func):
+    def wrapper(email, password):
+
+        password_rgx = r'[A-Za-z0-9@#$%^&+=]{8,}'
+        if not re.fullmatch(password_rgx, password):
+            raise Exception('Invalid password')
+
+        email_rgx = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        if not re.fullmatch(email_rgx, email):
+            raise Exception('Invalid email')
+
+        func(email, password)
+    return wrapper
+
+        
+
+
+def login(email: str, password: str):
+    with open(users_file, 'r') as f:
+        credentials = f'{email}:{password}'
+        if credentials in f.read():
+            print('Logged in')
+        else:
+            print('Invalid login or password')
+
+
+@validate_credentials
+def registration(email: str, password: str):
+    with open(users_file, "a") as f:
+        credentials = f'{email}:{password}\n'
+        f.writelines([credentials])
+
+
 def log_or_reg():
-    x = input("Напишите log для авторизации или reg для регистрации:")
-    if x == "reg":
-        with open("reg_file.csv", "a", newline = '') as f:
-            login = input("Введите email:")
-            password = input("Введите пароль:")
-            data = [login, password]
-            writer = csv.writer(f)
-            writer.writerow(data)                
-    elif x == "log":
-        with open("reg_file.json") as write_fail:
-            user_fail = json.dumps(write_fail.read())
-            login = input("Введите email:")
-            password = input("Введите пароль:")
-            txt = f'{login}:{password}'
-            for i in user_fail:
-                if txt == i:
-                    print("Авторизация прошла успешно")
-                else:
-                    print("Неправильное имя пользователя или пароль!")
+    x = input("Напишите 1 для авторизации или 2 для регистрации:")
+    if x == "2":
+        email = input("Введите email: ")
+        password = input("Введите пароль: ")
+        registration(email, password)        
+    elif x == "1":
+        email = input("Введите email: ")
+        password = input("Введите пароль: ")
+        login(email, password)
     else:
         print("Недопустимое значение, попробуйте снова!")
-        
+
 
 log_or_reg()
